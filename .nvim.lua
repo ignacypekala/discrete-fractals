5e03e4e44713abee4c99429104fd8f825065fc56
Ignacy Pękała <ignacy@pekala.eu>
2026-05-23 22:12:47 +0200

    Tweak neovim project config

--- .nvim.lua
+++ .nvim.lua
@@ -1,11 +1,13 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "asm",
     callback = function()
-        print("hello")
+        vim.opt_local.makeprg = "make"
+        vim.opt_local.colorcolumn = "100"
         vim.opt_local.expandtab = true
-        vim.opt_local.varsofttabstop = "8,32,8"
+        vim.opt_local.varsofttabstop = "8,8,24,8"
         vim.opt_local.shiftwidth = 0
         vim.opt_local.indentexpr = ""
     end,
     group = vim.api.nvim_create_augroup("ProjectNasmConfig", { clear = true })
 })
+
