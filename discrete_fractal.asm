ab244a10f3e70e275ad891f8e3e51b7fe44bcf82
Ignacy Pękała <ignacy@pekala.eu>
2026-05-23 22:21:51 +0200

    Get heap start

--- discrete_fractal.asm
+++ discrete_fractal.asm
@@ -2,6 +2,7 @@ global _start
 
 SYS_EXIT equ 60
 SYS_READ equ 0
+SYS_BRK equ 12
 SUCCESS_EXIT_CODE equ 0
 ERROR_EXIT_CODE equ 1
 
@@ -9,6 +10,10 @@ _start:
     pop         rdi                     ; Get parameter count
     sub         rdi, 2                  ; Expect program name and iteration count
     jnz         .error
+
+    mov         rax, SYS_BRK 
+    xor         edi, edi
+    syscall                             ; Get current heap start 
 
     mov         eax, SYS_EXIT
     xor         edi, edi
