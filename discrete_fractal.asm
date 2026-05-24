ff8aa06e5f0dde55fd9a1242ef126b2e6d89ff88
Ignacy Pękała <ignacy@pekala.eu>
2026-05-24 11:58:27 +0200

    Add untested reallocation

--- discrete_fractal.asm
+++ discrete_fractal.asm
@@ -4,6 +4,7 @@ SYS_EXIT        equ 60
 SYS_READ        equ 0
 SYS_BRK         equ 12
 SYS_MMAP        equ 9
+SYS_MREMAP      equ 25
 
 PROT_READ       equ 0x1
 PROT_WRITE      equ 0x2
@@ -29,38 +30,41 @@ _start:
     cmp         rdi, PARAMETER_COUNT
     jnz         .error
 
-    ; Allocate input string buffer
+    ; Allocate the string buffer
     mov         rax, SYS_MMAP
-    mov         edi, 0                          ; address hint
+    xor         edi, edi                          ; address hint
     mov         esi, INITIAL_BUFFER_SIZE
     mov         edx, PROT_READ | PROT_WRITE
     mov         r10, MAP_PRIVATE | MAP_ANONYMOUS
-    xor         r8, r8                          ; file descriptor
+    mov         r8, -1                          ; file descriptor
     xor         r9, r9                          ; offset
     syscall
     cmp         rax, MAP_FAILED
     jz          .error
 
-    mov         r9, rax                         ; input string buffer address
+    mov         r9, rax                         ; string buffer address
     push        r10
     mov         r10, INITIAL_BUFFER_SIZE_HALF
 
-.read_input:
+.load_initial_string_chunk:
     mov         rax, SYS_READ
     mov         edi, STD_IN
     mov         rsi, [r9 + r10]                 ; the free half of the buffer
     mov         rdx, r10                        ; the size of it
     syscall
-
     cmp         rax, READ_ERROR
     jz          .input_error
-
     cmp         rax, r10
-    jb          .input_loaded                   ; all the input was read 
-    
-    shl         r10, 1                          ; increase the buffer
+    jb          .input_loaded                   ; All the input was read.
 
-    ; TODO: Reallocate the buffer
+    ; Reallocate the string buffer
+    mov rax, SYS_MREMAP
+    xor         rdi, r9                         ; address
+    mov         rsi, r10                        ; old length
+    mov         rdi, rsi
+    shl         rdi, 1                          ; new length
+    mov         edi, MAP_PRIVATE | MAP_ANONYMOUS
+    xor         r8, r8                          ; new address hint
 
 .input_loaded:
 
