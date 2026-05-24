fbbb3d41f6bcecaa051f346131d4c2c7abb9e081
Ignacy Pękała <ignacy@pekala.eu>
2026-05-24 21:57:20 +0200

    Add first linebreak scan

--- discrete_fractal.asm
+++ discrete_fractal.asm
@@ -21,6 +21,8 @@ STD_OUT         equ 1
 INITIAL_BUFFER_SIZE         equ 4096
 INITIAL_BUFFER_SIZE_HALF    equ 2048
 
+LINE_BREAK      equ 10
+
 PARAMETER_COUNT equ 2                           ; program name + iteration count
 ERROR_CODE      equ 1
 
@@ -28,11 +30,12 @@ _start:
     ; Validate parameter count.
     pop         rdi
     cmp         rdi, PARAMETER_COUNT
-    jnz         .error
+    jnz         .error_exit
 
     ; Allocate the string buffer
+    ; TODO: Manage the address hints
     mov         rax, SYS_MMAP
-    xor         edi, edi                          ; address hint
+    xor         edi, edi                        ; address hint
     mov         esi, INITIAL_BUFFER_SIZE
     mov         edx, PROT_READ | PROT_WRITE
     mov         r10, MAP_PRIVATE | MAP_ANONYMOUS
@@ -40,41 +43,75 @@ _start:
     xor         r9, r9                          ; offset
     syscall
     cmp         rax, MAP_FAILED
-    jz          .error
+    jz          .error_exit
 
-    mov         r9, rax                         ; string buffer address
+    mov         r11, rax                         ; string buffer address
     mov         r10, INITIAL_BUFFER_SIZE_HALF
 
 .load_initial_string_chunk:
     mov         rax, SYS_READ
     mov         edi, STD_IN
-    lea         rsi, [r9 + r10]                 ; the free half of the buffer
-    mov         rdx, r10                        ; the size of it
+    lea         rsi, [r11 + r10]                 ; the free half of the buffer
+    mov         rdx, r10                        ; the size of the empty half
     syscall
     cmp         rax, READ_ERROR
-    jz          .input_error
-    cmp         rax, r10
-    jb          .input_loaded                   ; All the input was read.
+    jz          .free_string_buffer_and_quit
+
+    test        rax, rax
+    jz          .free_string_buffer_and_quit      ; The input ended before the first line break.
+
+    ; Check if the first line has been loaded completely
+    mov         rcx, rax                        ; the number of bytes read
+    lea         rdi, [r11 + r10]                 ; the start of last read chunk
+    mov         al, LINE_BREAK
+    repne       scasb
+    jz          .extract_rules_from_string_buffer       ; The line break has been found.
+
+    ; Increase the buffer size
+    shl         r10, 1
 
     ; Reallocate the string buffer
-    mov rax, SYS_MREMAP
-    xor         rdi, r9                         ; address
-    mov         rsi, r10                        ; old length
+    mov         rax, SYS_MREMAP
+    xor         rdi, r11                         ; string buffer address
+    mov         rsi, r10                        ; string buffer size
     mov         rdi, rsi
-    shl         rdi, 1                          ; new length
+    shl         rdi, 1                          ; new size
     mov         edi, MAP_PRIVATE | MAP_ANONYMOUS
-    xor         r8, r8                          ; new address hint
+    xor         r8,  r8                         ; new address hint
+    syscall
+    test        rax, rax
+    jz          .free_string_buffer_and_quit
+    mov         r11, rax
 
-.input_loaded:
+    jmp .load_initial_string_chunk
+
+.extract_rules_from_string_buffer:
+    ; rdi holds the address of the first byte of the rules (provided there are any).
+    ; rcx holds the length of the loaded rules.
+
+    ; Allocate the rules buffer
+    mov         rax, SYS_MMAP
+    xor         edi, edi                        ; address hint
+    mov         esi, INITIAL_BUFFER_SIZE
+    mov         edx, PROT_READ | PROT_WRITE
+    mov         r10, MAP_PRIVATE | MAP_ANONYMOUS
+    mov         r8, -1                          ; file descriptor
+    xor         r9, r9                          ; offset
+    syscall
+    cmp         rax, MAP_FAILED
+    jz          .free_string_buffer_and_quit
+
+.load_remaining_rules:
+    ; TODO: Load remaining rules
 
     mov         eax, SYS_EXIT
     xor         edi, edi
     syscall
 
-.input_error:
+.free_string_buffer_and_quit:
     ; TODO: Free the input buffer
     
-.error:
+.error_exit:
     mov         eax, SYS_EXIT
     mov         edi, ERROR_CODE
     syscall
