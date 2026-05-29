59f7c650651408f8484006e34fa6fb85306301f9
Ignacy Pękała <ignacy@pekala.eu>
2026-05-29 17:14:09 +0200

    Update vartabstop

--- .nvim.lua
+++ .nvim.lua
@@ -2,9 +2,10 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = "asm",
     callback = function()
         vim.opt_local.makeprg = "make"
-        vim.opt_local.colorcolumn = "100"
+        vim.opt_local.textwidth = 100
+        vim.opt_local.colorcolumn = "+1"
         vim.opt_local.expandtab = true
-        vim.opt_local.varsofttabstop = "8,8,32,8"
+        vim.opt_local.varsofttabstop = "8,16,32,8"
         vim.opt_local.shiftwidth = 0
         vim.opt_local.indentexpr = ""
     end,
