4f1fbfef66d23eef99cce59bac0fd85095f0bece
Ignacy Pękała <ignacy@pekala.eu>
2026-06-03 11:21:03 +0200

    Correct write buffer size

--- discrete_fractal.asm
+++ discrete_fractal.asm
@@ -27,7 +27,7 @@ FRAME_SIZE              equ 4096
 
 INITIAL_BUFFER_SIZE     equ FRAME_SIZE
 
-WRITE_BUFFER_SIZE       equ 64 * FRAME_SIZE
+WRITE_BUFFER_SIZE       equ 64 * 1000
 
 RULE_REGISTRY_ENTRY_SIZE        equ 8 * 2                  ; base pointer + rule length
 RULE_REGISTRY_TOTAL_SIZE        equ (ASCII_END - ASCII_START + 1) * RULE_REGISTRY_ENTRY_SIZE
